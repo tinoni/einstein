d0fe24528fc6e141e55766cb64049106562fa18a
Manuel Fernandes <mfernandes777@gmail.com>
2020-04-09 19:58:51 +0100

    Version with content_scripts

---
+++ scramble_page.js
@@ -0,0 +1,43 @@
+
+scramble = function(text) {
+    // 7H3 M345UR3 0F 1N73LL163NC3 15 7H3 481L7Y 70 CH4N63.
+    // 4L83R7 31N5731N
+    translations = {
+        't': '7',
+        'e': '3',
+        'a': '4',
+        's': '5',
+        'o': '0',
+        'i': '1',
+        'g': '6',
+        'b': '8',
+    };
+
+    for (key in translations) {
+        re = new RegExp(key, "gi");
+        text = text.replace(re, translations[key]);
+    }
+
+    return text;
+};
+
+
+
+scramble_page = function() {
+    $('body :not(script)').contents().filter(function() {
+        return this.nodeType === 3;
+    }).replaceWith(function() {
+        return scramble(this.nodeValue);
+    });
+}
+
+
+
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "clicked_browser_action" ) {
+            scramble_page();
+        }
+    }
+);
